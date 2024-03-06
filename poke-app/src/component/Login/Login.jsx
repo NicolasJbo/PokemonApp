09ff81b0daaee283b6b75cf0a20a6d129dc7e54f
NicolasJbo <57155671+NicolasJbo@users.noreply.github.com>
2024-03-05 21:55:01 -0300

    Login component

---
+++ poke-app/src/component/Login/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Login.css'
+
+
+export default function Login(){
+
+    return(
+        <>
+        <div className="container">
+            <div className="login-container"></div>
+        </div>
+        </>
+    );
+}
+
